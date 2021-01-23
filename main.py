785065a77368e7066757a689a056105fcd4914f0
NiranthS <sirusalansai.ece18@itbhu.ac.in>
2021-01-24 00:10:56 +0530

    .

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ for i in runs:
 			print(k)
 			for file in glob.glob('*info.mat'):
 				print(file)
+				import pdb; pdb.set_trace()
 				path_main = path+'/train/'+i+'/'+seq_name+'/'
 				mat = scipy.io.loadmat(path_main + file)
 				video_path = path_main + mat['sequence']+'.mp4'
