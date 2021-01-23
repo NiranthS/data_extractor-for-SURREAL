ff090949bc9f44fd209092d8861a4ef67cddda10
NiranthS <sirusalansai.ece18@itbhu.ac.in>
2021-01-23 23:15:06 +0530

    first commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from os import listdir
 import sys
 sys.path.append('/home/niranth/Desktop/projects/datasets/cmu')
-
-runs = listdir('train')
+path = '/home/niranth/Desktop/projects/datasets/cmu'
+runs = listdir(path+'/train')
 
 for i in runs:
 	print(i)
